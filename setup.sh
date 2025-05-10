a62e2ede2e996ea511657b620c1a173eee53d852
Immanuel S <immanuels999@gmail.com>
2025-05-10 14:31:49 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install Rust compiler for tokenizers
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-# Upgrade pip to the latest version
-pip install --upgrade pip
